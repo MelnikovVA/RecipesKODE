3c9c789762a09bee12ba06f1a7128f6dbffb7c23
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-07-05 21:30:32 +0700

    Some progress on the adapter
    Need to implement onBindViewHolder method

--- app/src/main/java/com/example/recipeskode/adapters/RecipeAdapter.kt
+++ app/src/main/java/com/example/recipeskode/adapters/RecipeAdapter.kt
@@ -5,20 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recipeskode.R
 import com.example.recipeskode.models.Recipe
+import kotlinx.android.synthetic.main.recipe_item_list.view.*
 
 class RecipeAdapter(val context: Context, val recipes: List<Recipe>) :
     RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.recipe_list_item, parent, false)
-    }
 
     override fun getItemCount(): Int {
         return recipes.size
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recipe_item_list, parent, false)
+        return RecipeViewHolder(view)
+    }
+
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val recipe = recipes[position]
     }
 
     inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
