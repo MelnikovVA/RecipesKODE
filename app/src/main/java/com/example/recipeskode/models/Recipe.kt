0e49c345fff86923c3da9cccb8f963ded2ad64f3
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-07-05 15:21:18 +0700

    Update model

--- app/src/main/java/com/example/recipeskode/models/Recipe.kt
+++ app/src/main/java/com/example/recipeskode/models/Recipe.kt
@@ -1,4 +1,11 @@
 package com.example.recipeskode.models
 
-class Recipe {
-}
+data class Recipe(
+    val uuid: String,
+    val name: String,
+    val images: Array<String>,
+    val lastUpdated: Int,
+    val description: String? = null,
+    val instructions: String,
+    val difficulty: Int
+)
