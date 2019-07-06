22a42658bcf745d5b62cb46988f4155ccd98e24c
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-07-06 15:50:18 +0700

    Little UI update

--- app/src/main/java/com/example/recipeskode/adapters/RecipeAdapter.kt
+++ app/src/main/java/com/example/recipeskode/adapters/RecipeAdapter.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipeskode.R
 import com.example.recipeskode.models.Recipe
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recipe_item_list.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RecipeAdapter(val context: Context, val recipes: List<Recipe>) :
     RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
@@ -27,9 +30,12 @@ class RecipeAdapter(val context: Context, val recipes: List<Recipe>) :
         holder?.itemView?.textCardName?.text = recipe.name
         holder?.itemView?.textCardDescription?.text = recipe.description
 
-        val sdf = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-        val date = java.util.Date(recipe.lastUpdated.toLong() * 1000)
-        holder?.itemView?.textCardLastUpdated.text = sdf.format(date)
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val date = Date(recipe.lastUpdated.toLong() * 1000)
+        holder?.itemView?.textCardLastUpdated.text = "Last Updated: ${sdf.format(date)}"
+
+        val recipePreviewImage = holder?.itemView?.imageRecipe
+        Picasso.get().load(recipe.images[0]).into(recipePreviewImage)
 
 
     }
